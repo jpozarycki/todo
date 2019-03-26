d098ea7b16bd851cbb1e3201b40f5cd6de7bfd54
jpozarycki <jakub.pozarycki@gmail.com>
2019-03-26 20:07:13 +0100

    Added customized message from Spring Boot application.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {WelcomeDataService} from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -9,9 +10,10 @@ import {ActivatedRoute} from '@angular/router';
 export class WelcomeComponent implements OnInit {
 
   message = 'Some Welcome Message';
+  welcomeMessageFromService: string;
   name = '';
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private welcomeDataService: WelcomeDataService) { }
 
   ngOnInit() {
     console.log(this.message);
@@ -19,4 +21,35 @@ export class WelcomeComponent implements OnInit {
     this.name = this.route.snapshot.params.name;
   }
 
+  // getWelcomeMessage() {
+  //   // console.log(this.welcomeDataService.executeHelloWorldService());
+  //   this.welcomeDataService.executeHelloWorldService().subscribe(
+  //     response => this.handleSuccessfulResponse(response),
+  //     error1 => this.handleErrorResponse(error1)
+  //   );
+  //
+  //   // console.log('last line of getWelcomeMessage');
+  // }
+  getWelcomeMessageWithParameter() {
+    // console.log(this.welcomeDataService.executeHelloWorldService());
+    this.welcomeDataService.executeHelloWorldWithPathVariableService(this.name).subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error1 => this.handleErrorResponse(error1)
+    );
+
+    // console.log('last line of getWelcomeMessage');
+  }
+
+  private handleSuccessfulResponse(response) {
+    this.welcomeMessageFromService = response.message;
+    // console.log(response);
+    // console.log(response.message);
+  }
+
+  private handleErrorResponse(error) {
+    this.welcomeMessageFromService = error.error.message;
+    // console.log(error);
+    // console.log(error.error);
+    // console.log(error.error.message);
+  }
 }
